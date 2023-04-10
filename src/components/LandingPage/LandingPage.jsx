2726b1c834e633bb074310de01b14aafe26653a6
bendaprile <benjamin.daprile@gmail.com>
2023-04-10 11:41:04 -0400

    add app logo to the landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
+import logo from "../../images/legalbeagles.jpeg";
 import {TextField} from "@mui/material";
 
 function LandingPage() {
     return <div data-testid="landing-page">
+        <img src={logo} className="logo" alt="logo" data-testid="app-logo" />
         <TextField id="name" data-testid="name-input" label="Name" variant="outlined" />
     </div>
 }

8e3001926db7c4229cf7e59f6686748024cac68f
chandlerkeyes123 <chandler.keyes@integral.io>
2023-04-10 10:30:54 -0500

    Added material UI, added name input field to LandingPage, added LandingPage to App.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LandingPage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const landingPage = screen.getByTestId("landing-page");
+  expect(landingPage).toBeInTheDocument();
 });
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
-function LandingPage() {
-    return <>
+import {TextField} from "@mui/material";
 
-    </>
+function LandingPage() {
+    return <div data-testid="landing-page">
+        <TextField id="name" data-testid="name-input" label="Name" variant="outlined" />
+    </div>
 }
 
 export default LandingPage;

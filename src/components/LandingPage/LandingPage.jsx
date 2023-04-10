952916bca4c205acc49fd99f1c67a92fba588a7b
Chris Daly <chris.daly@integral.io>
2023-04-10 14:11:31 -0500

    Refactor tests to remove test-ids

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,76 +1,81 @@
 import './LandingPage.css'
 import logo from "../../images/legalbeagles.jpeg";
-import {TextField} from "@mui/material";
+import { FormControl, InputLabel, TextField } from "@mui/material";
 import { DateField, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import {states} from "../../common/states";
+import { states } from "../../common/states";
 import MenuItem from '@mui/material/MenuItem';
-import Select  from '@mui/material/Select';
-import {useState} from "react";
+import Select from '@mui/material/Select';
+import { useState } from "react";
 import { callChatGPTAPI } from '../../common/api';
 import Button from '@mui/material/Button';
 
 function LandingPage() {
-    const [state, setState] = useState("");
-    const handleStateChange = (event) => {
-        setState(event.target.value)
-    }
+  const [state, setState] = useState("");
+  const handleStateChange = (event) => {
+    setState(event.target.value)
+  }
 
-    const handleSubmit = () => {
-        console.log("Clicked");
-        //TODO: Call chat gpt api here and go to next page.
-    }
+  const handleSubmit = () => {
+    console.log("Clicked");
+    //TODO: Call chat gpt api here and go to next page.
+  }
 
-    callChatGPTAPI("Hello world").then((response) => {
-        console.log(response);
-    }).catch((err) => {
-        console.error("oops no go" + err);
-    });
+  callChatGPTAPI("Hello world").then((response) => {
+    console.log(response);
+  }).catch((err) => {
+    console.error("oops no go" + err);
+  });
 
-    return <div data-testid="landing-page">
-        <div className={"header-area"}>
-            <img src={logo} className="logo" alt="logo" data-testid="app-logo" />
-            <h1>LegalEASE Intake Form</h1>
-            <p>Welcome to LegalEASE, powered by ChatGPT! Our intelligent chatbot will guide you through the process of providing us with the necessary information to help us understand your legal needs.
-            <br/><br/>Our goal is to make this process as easy and efficient as possible for you while ensuring that we gather all the necessary information to assist you.
-            <br/><br/>To get started, simply provide us with some basic information about yourself and a short summary of your legal matter. Our chatbot will review and ask any follow-up questions.
-            <br/><br/>Thank you for choosing Legal Beagles Inc.!</p>
-        </div>
-        <form className={"form-fields"}>
-            <TextField id="name" className="name-field" data-testid="name-input" label="Full Legal Name" variant="outlined" />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateField label="Birth Date" className="date-field" data-testid="birth-date"/>
-            </LocalizationProvider>
-            <TextField id="address-line-1" className="address-1-field" data-testid="address-line-1" label="Address Line 1" variant="outlined" />
-            <TextField id="address-line-2" className="address-2-field" data-testid="address-line-2" label="Address Line 2 (optional)" variant="outlined" />
-            <TextField id="city" className="city-field" data-testid="city" label="City" variant="outlined" />
-            <Select
-                id="state-select"
-                data-testid="state-select"
-                className="state-field"
-                value={state}
-                label="State"
-                onChange={handleStateChange}
-                displayEmpty={true}
-                renderValue={value => value || "Select State"}
-            >
-                {states.map((state, index) => {
-                    return <MenuItem value={state.value} key={index}>{state.text}</MenuItem>
-                })}
-            </Select>
-            <TextField id="zip-code" className="zip-field" data-testid="zip-code" label="Zip Code" variant="outlined" />
-            <TextField
-                id="summary"
-                className="summary-field"
-                data-testid="summary"
-                label="Summary"
-                multiline
-                rows={5}
-                defaultValue="Please provide a short summary of your legal matter with as much detail as possible so we can best assist you."
-            />
-            <Button className="submit-button" variant="contained" data-testid="submit-button" onClick={handleSubmit} size="large">Submit</Button>
-        </form>
+  return <div>
+    <div className={"header-area"}>
+      <img src={logo} className="logo" alt="logo" />
+      <h1>LegalEASE Intake Form</h1>
+      <p>Welcome to LegalEASE, powered by ChatGPT! Our intelligent chatbot will guide you through the process of
+        providing us with the necessary information to help us understand your legal needs.
+        <br /><br />Our goal is to make this process as easy and efficient as possible for you while ensuring that we
+        gather all the necessary information to assist you.
+        <br /><br />To get started, simply provide us with some basic information about yourself and a short summary of
+        your legal matter. Our chatbot will review and ask any follow-up questions.
+        <br /><br />Thank you for choosing Legal Beagles Inc.!</p>
     </div>
+    <form className={"form-fields"}>
+      <TextField id="name" className="name-field" label="Full Legal Name" variant="outlined" />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateField label="Birth Date" className="date-field" />
+      </LocalizationProvider>
+      <TextField id="address-line-1" className="address-1-field" label="Address Line 1" variant="outlined" />
+      <TextField id="address-line-2" className="address-2-field" label="Address Line 2 (optional)" variant="outlined" />
+      <TextField id="city" className="city-field" label="City" variant="outlined" />
+      <TextField
+        id="state-select"
+        select
+        label="State"
+        value={state}
+        onChange={handleStateChange}
+        variant="outlined"
+        color="secondary"
+        inputProps={{
+          id: "state-select"
+        }}
+        fullWidth
+      >
+        {states.map((state, index) => {
+          return <MenuItem value={state.value} key={index}>{state.text}</MenuItem>
+        })}
+      </TextField>
+      <TextField id="zip-code" className="zip-field" label="Zip Code" variant="outlined" />
+      <TextField
+        id="summary"
+        className="summary-field"
+        label="Summary"
+        multiline
+        rows={5}
+        defaultValue="Please provide a short summary of your legal matter with as much detail as possible so we can best assist you."
+      />
+      <Button className="submit-button" variant="contained" onClick={handleSubmit} size="large">Submit</Button>
+    </form>
+  </div>
 }
 
 export default LandingPage;
--- src/components/LandingPage/LandingPage.test.jsx
+++ src/components/LandingPage/LandingPage.test.jsx
@@ -3,62 +3,63 @@ import LandingPage from "./LandingPage";
 
 test('renders name input field', () => {
     render(<LandingPage />);
-    const nameInput = screen.getByTestId("name-input");
+    const nameInput = screen.getByLabelText("Full Legal Name");
     expect(nameInput).toBeInTheDocument();
 });
 
 test('renders logo', () => {
     render(<LandingPage />);
-    const appLogo = screen.getByTestId("app-logo");
+    const appLogo = screen.getByRole("img");
     expect(appLogo).toBeInTheDocument();
+    expect(appLogo).toHaveAttribute("alt", "logo");
 });
 
 test('renders birth-date input field', () => {
     render(<LandingPage />);
-    const birthDate = screen.getByTestId("birth-date");
+    const birthDate = screen.getByLabelText("Birth Date");
     expect(birthDate).toBeInTheDocument();
 });
 
 test('renders Address Line 1 input field', () => {
     render(<LandingPage />);
-    const addressLine1 = screen.getByTestId("address-line-1");
+    const addressLine1 = screen.getByLabelText("Address Line 1");
     expect(addressLine1).toBeInTheDocument();
 });
 
 test('renders Address Line 2 input field', () => {
     render(<LandingPage />);
-    const addressLine1 = screen.getByTestId("address-line-2");
+    const addressLine1 = screen.getByLabelText("Address Line 2 (optional)");
     expect(addressLine1).toBeInTheDocument();
 });
 
 test('renders city input field', () => {
     render(<LandingPage />);
-    const addressLine2 = screen.getByTestId("city");
+    const addressLine2 = screen.getByLabelText("City");
     expect(addressLine2).toBeInTheDocument();
 });
 
 test('renders state select field', () => {
     render(<LandingPage />);
-    const stateSelect = screen.getByTestId("state-select");
+    const stateSelect = screen.getByLabelText("State");
     expect(stateSelect).toBeInTheDocument();
-    expect(stateSelect).toHaveTextContent("Select State");
 });
 
 test('renders zip input field', () => {
     render(<LandingPage />);
-    const zipCode = screen.getByTestId("zip-code");
+    const zipCode = screen.getByLabelText("Zip Code");
     expect(zipCode).toBeInTheDocument();
 });
 
 test('renders summary field', () => {
     render(<LandingPage />);
-    const summary = screen.getByTestId("summary");
+    const summary = screen.getByLabelText("Summary");
     expect(summary).toBeInTheDocument();
+    expect(summary).toHaveDisplayValue("Please provide a short summary of your legal matter with as much detail as possible so we can best assist you.")
 });
 
 test('renders submit button', () => {
     render(<LandingPage />);
-    const submitButton = screen.getByTestId("submit-button");
+    const submitButton = screen.getByRole("button", {name: "Submit"});
     expect(submitButton).toBeInTheDocument();
 });
 
